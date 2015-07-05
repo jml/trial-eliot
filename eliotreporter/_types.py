bcaaaf39d86f2706d793bad0f8253cbc4451548e
Jonathan Lange <jml@mumak.net>
2015-07-05 16:05:51 +0100

    Documentation

--- eliotreporter/_types.py
+++ eliotreporter/_types.py
@@ -23,6 +23,9 @@ from twisted.python.failure import Failure
 _TEST = Field(u'test', lambda test: test.id(), u'The test')
 
 
+"""
+The action of running a test.
+"""
 TEST = ActionType(u'trial:test',
                   [_TEST],
                   [],
@@ -40,11 +43,21 @@ _REASON = Field(u'reason', unicode, 'The reason for the raised exception')
 _TRACEBACK = Field(u'traceback', unicode, 'The traceback')
 
 
+"""
+Logged when an error occurs in a test.
+"""
 ERROR = MessageType(u'trial:test:error', [_EXCEPTION, _REASON, _TRACEBACK])
+
+"""
+Logged when an assertion fails in a test.
+"""
 FAILURE = MessageType(u'trial:test:failure', [_EXCEPTION, _REASON, _TRACEBACK])
 
 
 def _failure_to_exception_tuple(failure):
+    """
+    Convert a ``Failure`` to an exception 3-tuple.
+    """
     return (
         failure.value.__class__,
         failure.value,
@@ -53,12 +66,26 @@ def _failure_to_exception_tuple(failure):
 
 
 def _adapt_to_exception_tuple(failure):
+    """
+    Take whatever failure trial can throw at us and return an exception
+    3-tuple.
+    """
     if isinstance(failure, Failure):
         return _failure_to_exception_tuple(failure)
     return failure
 
 
 def make_error_message(message_type, failure):
+    """
+    Create a message for an error or failure in a test.
+
+    :param message_type: A function that returns a ``Message`` and takes an
+        ``exception``, a ``reason``, and a ``traceback``.
+    :param failure: The failure that occurred. Either a
+        ``twisted.python.failure.Failure`` or an exception 3-tuple.
+
+    :return: An Eliot ``Message``.
+    """
     exc_type, exc_value, exc_traceback = _adapt_to_exception_tuple(failure)
     return message_type(
         exception=exc_type,
